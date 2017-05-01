75c973ce39da920b0e045e424939656cf5d255d3
Patrick <bzprod@gmail.com>
2017-05-01 15:24:37 -0700

    Update microsoft_graph.rb

--- lib/omniauth/strategies/microsoft_graph.rb
+++ lib/omniauth/strategies/microsoft_graph.rb
@@ -6,9 +6,9 @@ module OmniAuth
       option :name, :microsoft_graph
 
       option :client_options, {
-        site:          'https://login.microsoftonline.com/common/oauth2/authorize',
-        token_url:     'https://login.microsoftonline.com/common/oauth2/token',
-        authorize_url: 'https://login.microsoftonline.com/common/oauth2/authorize'
+        site:          'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
+        token_url:     'https://login.microsoftonline.com/common/oauth2/v2.0/token',
+        authorize_url: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize'
       }
 
       option :authorize_params, {
